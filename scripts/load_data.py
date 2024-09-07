673745eea0d6f7876b164a5c00bc6bc1f695362b
Birkity <85987355+Birkity@users.noreply.github.com>
2024-09-07 11:51:14 +0300

    Load the data from postgreSQL and minor edits

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -11,11 +11,11 @@ from sqlalchemy import create_engine
 load_dotenv()
 
 # Fetch database connection parameters from environment variables
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST").strip()
+DB_PORT = os.getenv("DB_PORT").strip()
+DB_NAME = os.getenv("DB_NAME").strip()
+DB_USER = os.getenv("DB_USER").strip()
+DB_PASSWORD = os.getenv("DB_PASSWORD").strip()
 
 def load_data_from_postgres(query):
     """
@@ -28,7 +28,7 @@ def load_data_from_postgres(query):
         # Establish a connection to the database
         connection = psycopg2.connect(
             host=DB_HOST,
-            port=DB_PORT,
+            port=int(DB_PORT),
             database=DB_NAME,
             user=DB_USER,
             password=DB_PASSWORD
